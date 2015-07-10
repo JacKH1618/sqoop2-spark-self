b95d84d2f81db0e61d75af1c76947e9dc16f6bab
Banmeet Singh <banmeet.singh@cloudera.com>
2015-07-09 19:47:35 -0700

    Just forgot to add, so committing again
    Got rid of the SparkSubmissionEngine class not getting instantiated issue (a pom issue; just use poms in ~/sqoop, spark-engine branch; see Spark notes) But failing right now - have to add config stuff in sqoop.properties with the spark engine tag (as that's what the prefix string is going to be)

--- submission/spark/src/main/java/org/apache/sqoop/submission/spark/SparkSubmissionEngine.java
+++ submission/spark/src/main/java/org/apache/sqoop/submission/spark/SparkSubmissionEngine.java
@@ -55,14 +55,25 @@ import org.apache.sqoop.submission.counter.CounterGroup;
 import org.apache.sqoop.submission.counter.Counters;
 */
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.io.Serializable;
 
 import org.apache.sqoop.driver.SubmissionEngine;
 import org.apache.sqoop.common.MapContext;
 import org.apache.sqoop.driver.JobRequest;
 import org.apache.sqoop.model.MSubmission;
-import org.apache.sqoop.execution.spark.SparkExecutionEngine;
+//import org.apache.sqoop.execution.spark.SparkExecutionEngine;
 import org.apache.log4j.Logger;
+import org.apache.sqoop.common.MapContext;
+import org.apache.sqoop.error.code.SparkSubmissionError;
+import org.apache.sqoop.common.SqoopException;
+
+//Hadoop imports
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.JobConf;
 
 //Apache Spark imports
 import org.apache.spark.api.java.*;
@@ -84,7 +95,8 @@ public class SparkSubmissionEngine extends SubmissionEngine implements Serializa
    * Global configuration object that is build from hadoop configuration files
    * on engine initialization and cloned during each new submission creation.
    */
-  //private Configuration globalConfiguration;
+  private Configuration globalConfiguration;
+  private JobConf jobConf;
 
 
   /**
@@ -95,11 +107,52 @@ public class SparkSubmissionEngine extends SubmissionEngine implements Serializa
     super.initialize(context, prefix);
     LOG.info("Initializing Spark Submission Engine");
 
-    /*
-    sparkJobConf = new SparkConf().setAppName("Sqoop on Spark").setMaster("local");
-    sparkJavaContext = new JavaSparkContext(sparkJobConf);
-    */
+    // Build global configuration, start with empty configuration object
+    globalConfiguration = new Configuration();
+    globalConfiguration.clear();
+
+    // Load configured hadoop configuration directory
+    String configDirectory = context.getString(prefix + Constants.CONF_CONFIG_DIR);
+
+    // Git list of files ending with "-site.xml" (configuration files)
+    File dir = new File(configDirectory);
+    String [] files = dir.list(new FilenameFilter() {
+      @Override
+      public boolean accept(File dir, String name) {
+        return name.endsWith("-site.xml");
+      }
+    });
+
+    if(files == null) {
+      throw new SqoopException(SparkSubmissionError.SPARK_0002,
+          "Invalid Hadoop configuration directory (not a directory or permission issues): " + configDirectory);
+    }
+
+    // Add each such file to our global configuration object
+    for (String file : files) {
+      LOG.info("Found hadoop configuration file " + file);
+      try {
+        globalConfiguration.addResource(new File(configDirectory, file).toURI().toURL());
+      } catch (MalformedURLException e) {
+        LOG.error("Can't load configuration file: " + file, e);
+      }
+    }
+
+    // Save our own property inside the job to easily identify Sqoop jobs
+    globalConfiguration.setBoolean(Constants.SQOOP_JOB, true);
+
+    // Create jobConf object to be used for creating the RDD from InputFormat
+    //try {
+      jobConf = new JobConf(globalConfiguration);
+      //jobClient = new JobClient(new JobConf(globalConfiguration));
+    //} catch (IOException e) {
+      //throw new SqoopException(SparkSubmissionError.SPARK_0002, e);
+    //}
+
+    //if(isLocal()) {
+    //LOG.info("Detected MapReduce local mode, some methods might not work correctly.");
   }
+
 
   /**
    * {@inheritDoc}
@@ -114,8 +167,8 @@ public class SparkSubmissionEngine extends SubmissionEngine implements Serializa
    */
   @Override
   public boolean isExecutionEngineSupported(Class<?> executionEngineClass) {
-    return executionEngineClass == SparkExecutionEngine.class;
-    //return true;
+    //return executionEngineClass == SparkExecutionEngine.class;
+    return true;
   }
 
   /**
